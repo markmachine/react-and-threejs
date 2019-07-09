f5a3b37c3b4e23fa15b02f152289505c27da1725
Mark R. Hughes <mrhughes@xogrp.com>
2019-07-09 10:58:49 -0500

    base commit of tutorial example code

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
+import React, { Component } from "react";
+import ReactDOM from "react-dom";
+import * as THREE from "three";
 import logo from './logo.svg';
 import './App.css';
+import scene from "./threejs.scene.js"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    scene(this);
+  }
+  render() {
+    return (
+      <div ref={ref => (this.mount = ref)} />
+    )
+  }
 }
 
 export default App;
